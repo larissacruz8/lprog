1f6d67a7b217ffb6572f3bcfb4c6af52b2bddfeb
Larissa Cruz <111009689+larissacruz8@users.noreply.github.com>
2022-09-21 21:09:22 +0000

    aula7

---
+++ Modelos/Endereco.cs
@@ -0,0 +1,21 @@
+namespace Modelos {
+
+  class Endereco : Contato {
+
+    public string Local {
+        get; set;
+      
+    }
+
+    public string Postal {
+        get; set;
+      
+    }
+    
+    public string Informar() {
+      return "Local:" + this.Local + "\n" +
+        "Postal:" + this.Postal;
+      
+    }
+  }
+}
